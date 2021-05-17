fac08f83c60682b3a19b870363c701aa7e75b3a6
SimonFangCisco <sifang@cisco.com>
2021-05-17 11:42:19 +0200

    Added two more functions reading from the path directly

--- labs/secure-network-analytics-pandas-labs/assets/pandas_excercise.py
+++ labs/secure-network-analytics-pandas-labs/assets/pandas_excercise.py
@@ -3,13 +3,24 @@ import pandas as pd
 import json
 import os
 
+script_directory = os.path.dirname(__file__) #absolute dir of script
+file_name_json = 'flow-results.json'
+abs_file_path_json = os.path.join(script_directory, file_name_json)
+file_name_csv = 'flow-results.csv'
+abs_file_path_csv = os.path.join(script_directory, file_name_csv)
+
 def obtain_df():
-    script_directory = os.path.dirname(__file__) #absolute dir of script
-    file_name = 'flow-results.json'
-    abs_file_path = os.path.join(script_directory, file_name)
-    with open(abs_file_path) as file:
+    with open(abs_file_path_json) as file:
         flow_data = json.load(file)
     df = pd.DataFrame.from_dict(flow_data)
+    return df
+
+def obtain_df_from_json():
+    df = pd.read_json(abs_file_path_json)
+    return df
+
+def obtain_df_from_csv():
+    df = pd.read_csv(abs_file_path_csv)
     return df
 
 if __name__ == '__main__':
