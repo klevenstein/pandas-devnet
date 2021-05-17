881505d863d4c9ce5ef02d0759fd1d9394273ca6
SimonFangCisco <sifang@cisco.com>
2021-05-17 10:59:53 +0200

    Updated the pandas_exercise.py file, so the function can be re-used for 2nd exercise

--- labs/secure-network-analytics-pandas-labs/assets/pandas_excercise.py
+++ labs/secure-network-analytics-pandas-labs/assets/pandas_excercise.py
@@ -3,14 +3,15 @@ import pandas as pd
 import json
 import os
 
-def main():
+def obtain_df():
     script_directory = os.path.dirname(__file__) #absolute dir of script
     file_name = 'flow-results.json'
     abs_file_path = os.path.join(script_directory, file_name)
     with open(abs_file_path) as file:
         flow_data = json.load(file)
     df = pd.DataFrame.from_dict(flow_data)
-    print(df.head())
+    return df
 
 if __name__ == '__main__':
-    main()
+    df = obtain_df()
+    print(df.head())
