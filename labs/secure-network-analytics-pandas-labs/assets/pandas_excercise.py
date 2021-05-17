d07df81eeab113cf8ebab3d7220bd3ddd23059b9
SimonFangCisco <sifang@cisco.com>
2021-05-17 10:42:16 +0200

    Updated pandas_exercise.py with code to open the file and save to df

--- labs/secure-network-analytics-pandas-labs/assets/pandas_excercise.py
+++ labs/secure-network-analytics-pandas-labs/assets/pandas_excercise.py
@@ -1,8 +1,16 @@
 import numpy as np
 import pandas as pd
+import json
+import os
 
 def main():
-    print("python main function")
+    script_directory = os.path.dirname(__file__) #absolute dir of script
+    file_name = 'flow-results.json'
+    abs_file_path = os.path.join(script_directory, file_name)
+    with open(abs_file_path) as file:
+        flow_data = json.load(file)
+    df = pd.DataFrame.from_dict(flow_data)
+    print(df.head())
 
 if __name__ == '__main__':
     main()
