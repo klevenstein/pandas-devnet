bc3de19055d5e780d8ea1efae4814846f951fa35
SimonFangCisco <sifang@cisco.com>
2021-05-20 23:23:35 +0200

    Added 6.md and the accompanying code

--- labs/secure-network-analytics-pandas-labs/assets/pandas_excercise.py
+++ labs/secure-network-analytics-pandas-labs/assets/pandas_excercise.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import json
 import os
+import numpy as np
 
 script_directory = os.path.dirname(__file__) #absolute dir of script
 file_name_json = 'flow-results.json'
@@ -21,7 +22,9 @@ def obtain_df_from_json():
 def obtain_df_from_csv():
     df = pd.read_csv(abs_file_path_csv)
     return df
-    
+
+## 4.md
+## Average bytes per flow    
 def text_to_num(text, bad_data_val = 0):
     letter_to_number_dict = {
         'K': 1000,
@@ -29,7 +32,7 @@ def text_to_num(text, bad_data_val = 0):
         'B': 1000000000
     }
     if not isinstance(text, str):
-        # Non-strings are bad are missing data in poster's submission
+        # Non-strings are missing data 
         return bad_data_val
     elif '--' in text:
         return bad_data_val
@@ -40,10 +43,31 @@ def text_to_num(text, bad_data_val = 0):
     else:
         return float(text)
 
-def average_byte_per_flow(df):
+def average_bytes_per_flow(df):
     df['Total Bytes'] = df['Total Bytes'].apply(text_to_num)
     average = df['Total Bytes'].mean()
     return average
+
+## 6.md
+## Calculate avereage bytes per flow per hostgroup
+
+def average_bytes_per_flow_per_hostgroup(df):
+    aggregates_dict = {}
+    for _, row in df.iterrows():
+        subject_peer_pair = row['Subject Host Groups'] + ":" + row['Peer Host Groups']
+        if subject_peer_pair in aggregates_dict:
+            aggregates_dict[subject_peer_pair].append(row['Total Bytes'])
+        else:
+            aggregates_dict[subject_peer_pair] = [row['Total Bytes']]
+
+    averages_dict = {}
+    for key in aggregates_dict:
+        total_bytes_per_hostgroup = aggregates_dict[key]
+        average_bytes_per_flow_per_hostgroup = np.mean(total_bytes_per_hostgroup)
+        averages_dict[key] = average_bytes_per_flow_per_hostgroup
+
+    return averages_dict
+
 
 if __name__ == '__main__':
     df = obtain_df()
